6611864758513cb2af89f9b733f1b462558d55ac
danJ <qiaojiahui118@gmail.com>
2022-08-02 22:24:54 +0800

    axios的初步封装

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,1 +1,26 @@
 import axios from 'axios'
+// 创建axios实例
+const http = axios.create({
+    baseURL: 'http://geek.itheima.net/v1_0/',
+    timeout: 10000
+})
+
+// 请求拦截器
+http.interceptors.request.use(
+    (config) => {
+        return config
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+)
+// 响应拦截器
+http.interceptors.response.use(
+    (response) => {
+        return response
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+)
+export default http
